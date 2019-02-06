57ba5c4cc68f26c3fbbbf90f19dc162ecfde0d1e
Guill <guill.kill@gmail.com>
2019-02-06 20:54:12 +0000

    section 6

--- gulpfile.js
+++ gulpfile.js
@@ -3,7 +3,9 @@ watch = require('gulp-watch'),
 postcss = require('gulp-postcss'),
 autoprefixer = require('autoprefixer'),
 cssvars = require('postcss-simple-vars')
-nested = require('postcss-nested');
+nested = require('postcss-nested'),
+cssImport = require('postcss-import'),
+browserSync = require('browser-Sync').create();
 
 gulp.task('default', function() {
   console.log("yeeep - worked")
@@ -15,12 +17,18 @@ gulp.task('html', function() {
 
 gulp.task('styles', function() {
   return gulp.src('./app/assets/styles/styles.css')
-    .pipe(postcss([cssvars, nested, autoprefixer]))
+    .pipe(postcss([cssImport, cssvars, nested, autoprefixer]))
     .pipe(gulp.dest('./app/temp/styles'));
 
 });
 
 gulp.task('watch', function() {
+
+  browserSync.init({
+    server: {
+      baseDir: "app"
+    }
+  });
 
   watch('./app/index.html', function() {
     gulp.start('html')
