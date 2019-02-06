7fb6848585fe1d1bc390ad5d438f533880a8aecc
Guill <guill.kill@gmail.com>
2019-02-06 11:55:44 +0000

    committed

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,23 @@
-var gulp = require('gulp');
+var gulp = require('gulp'),
+watch = require('gulp-watch');
 
 gulp.task('default', function() {
-  console.log("yeeep - worked");
+  console.log("yeeep - worked")
+});
+
+gulp.task('html', function() {
+  console.log("something happened here in html.")
+});
+
+gulp.task('styles', function() {
+  console.log("css altered.")
+})
+
+gulp.task('watch', function() {
+  watch('./app/index.html', function() {
+    gulp.start('html')
+  });
+  Watch('./app/assets/styles/styles.css', function() {
+    gulp.start('styles')
+  })
 });
